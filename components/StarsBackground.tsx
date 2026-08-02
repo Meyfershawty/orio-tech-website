8c7204e2428a5e17f43a77f11f6407aaf575a35b
Nikolsky Jamo <oriontech072@gmail.com>
2026-08-02 15:23:43 +0200

    Corrigir build

--- components/StarsBackground.tsx
+++ components/StarsBackground.tsx
@@ -1,51 +1,5 @@
 "use client";
 
-import Particles from "@tsparticles/react";
-
 export default function StarsBackground() {
-  return (
-    <Particles
-      id="stars"
-      className="absolute inset-0"
-      options={{
-        fullScreen: {
-          enable: false,
-        },
-        background: {
-          color: {
-            value: "transparent",
-          },
-        },
-        fpsLimit: 60,
-        detectRetina: true,
-        particles: {
-          number: {
-            value: 80,
-          },
-          color: {
-            value: "#3b82f6",
-          },
-          links: {
-            enable: true,
-            color: "#3b82f6",
-            distance: 150,
-            opacity: 0.25,
-          },
-          move: {
-            enable: true,
-            speed: 1,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          size: {
-            value: {
-              min: 1,
-              max: 3,
-            },
-          },
-        },
-      }}
-    />
-  );
+  return null;
 }
