7f047746e7f146a2276719fc0e432b9912f83aae
Nikolsky Jamo <oriontech072@gmail.com>
2026-08-02 16:16:43 +0200

    Adicionar Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "@/context/LanguageContext";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -124,6 +125,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-SL9JY87GVF"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-SL9JY87GVF');
+  `}
+</Script>
         <script
   type="application/ld+json"
   dangerouslySetInnerHTML={{
