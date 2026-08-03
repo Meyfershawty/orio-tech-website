0ce37876bf3ec26fa5cc6432bc9b8c691cb89adc
Nikolsky Jamo <oriontech072@gmail.com>
2026-08-03 02:01:05 +0200

    Corrigir sitemap para domínio Vercel

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://oriontech.co.mz";
+  const baseUrl = "https://orio-tech-website-wtgc.vercel.app";
 
   return [
     {
